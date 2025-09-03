29bd8323bd1f2bf5799c933f4e2766050974c6fd
Allan Sechrist <sechristallan@gmail.com>
2025-09-04 07:49:59 +0900

    adjust buttons on carousel

--- src/assets/pages/home-page.jsx
+++ src/assets/pages/home-page.jsx
@@ -3,12 +3,13 @@ import GameForm from "../components/GameForm";
 import GameCard from "../components/GameCard";
 import Carousel from "../components/Carousel";
 import { useGameList } from "../context/GameListContext";
+import { HiH1 } from "react-icons/hi2";
 
 const HomePage = () => {
   const { randomGame } = useGameList();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-base-300 space-y-4">
-      {randomGame ? <Carousel /> : ""}
+      {randomGame ? <Carousel /> : <h1 className="font-semibold text-4xl text-center">Find a Game</h1>}
 
       <GameForm />
     </div>
