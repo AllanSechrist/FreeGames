7b7b59688d8d7ca36efc793c531c309b9dd384b2
Allan Sechrist <sechristallan@gmail.com>
2025-09-08 07:48:51 +0900

    Position form and title in the center on home page

--- src/assets/pages/home-page.jsx
+++ src/assets/pages/home-page.jsx
@@ -8,8 +8,8 @@ import { useGameList } from "../context/GameListContext";
 const HomePage = () => {
   const {randomGame} = useGameList();
   return (
-    <div className="grid place-items-center min-h-screen bg-base-300">
-      {randomGame ? <GameCard game={randomGame} /> : <h2 className="text-lg font-semibold mb-2 text-center">Fill out the checkboxes and I'll help you find a new game to play!</h2>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-base-300 space-y-4">
+      {randomGame ? <GameCard game={randomGame} /> : <h1 className="text-4xl font-semibold mb-2 text-center">Find A Game</h1>}
       <GameForm />
     </div>
   );
