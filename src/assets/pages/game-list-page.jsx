3556a7945dba8b115d82c1f07dff8a9eb0d0d2b5
Allan Sechrist <sechristallan@gmail.com>
2025-09-08 08:05:12 +0900

    Add props games and emptyListMessage to increase reusablity of gamelist

--- src/assets/pages/game-list-page.jsx
+++ src/assets/pages/game-list-page.jsx
@@ -1,7 +1,11 @@
 import GameList from "../components/GameList";
+import { useGameList } from "../context/GameListContext";
 const GameListPage = () => {
+  const {savedGames} = useGameList();
+  const noGamesMessage = "Your saved games list is empty"
+
   return (
-    <GameList />
+    <GameList games={savedGames} emptyListMessage={noGamesMessage}/>
   );
 }
 
