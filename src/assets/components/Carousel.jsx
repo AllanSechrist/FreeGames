5f9d202ca7da31c71e402db4e9d0156a145c42b7
Allan Sechrist <sechristallan@gmail.com>
2025-09-08 07:44:42 +0900

    Remove carousel nav buttons.

--- src/assets/components/Carousel.jsx
+++ src/assets/components/Carousel.jsx
@@ -4,75 +4,30 @@ import { useGameList } from "../context/GameListContext";
 const Carousel = () => {
   const { randomGame } = useGameList();
   return (
-    <div className='w-135'>
+    <div className="w-135">
       <div className="carousel carousel-center rounded-box w-full">
         {/* {GameList.map((game) => {
-        <div className="carousel-item w-full">
-          <GameCard game={game}/>
-        </div>
-      } )} */}
+          <div className="carousel-item w-full">
+            <GameCard game={game}/>
+          </div>
+        } )} */}
         <div
           id="slide1"
           className="carousel-item relative w-full flex justify-center"
         >
           <GameCard game={randomGame} />
-
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a
-              href="#slide3"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide2"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❯
-            </a>
-          </div>
         </div>
         <div
           id="slide2"
           className="carousel-item relative w-full flex justify-center"
         >
           <GameCard game={randomGame} />
-
-          <div className="absolute left-5 right-5 top-1/3 flex -translate-y-1/2 transform justify-between">
-            <a
-              href="#slide1"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide3"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❯
-            </a>
-          </div>
         </div>
         <div
           id="slide3"
           className="carousel-item relative w-full flex justify-center"
         >
           <GameCard game={randomGame} />
-
-          <div className="absolute left-5 right-5 top-1/3 flex -translate-y-1/2 transform justify-between">
-            <a
-              href="#slide2"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❮
-            </a>
-            <a
-              href="#slide1"
-              className="btn btn-xl btn-outline btn-info btn-square"
-            >
-              ❯
-            </a>
-          </div>
         </div>
       </div>
     </div>
