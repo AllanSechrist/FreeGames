0952b10d1e307c9b76bfee300605083cf9a019b5
Allan Sechrist <sechristallan@gmail.com>
2025-09-11 07:33:01 +0900

    Add line-clamp to GameCard so cards have consistent height. Update home page button text to Find Games.

--- src/assets/pages/home-page.jsx
+++ src/assets/pages/home-page.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-base-300 space-y-4">
       {/* {randomGames ? <GameCard game={randomGames} /> : <h1 className="text-4xl font-semibold mb-2 text-center">Find A Game</h1>} */}
-      <GameList games={randomGames} emptyListMessage={<h1 className="text-4xl font-semibold mb-2 text-center">Find A Game</h1>} />
+      <GameList games={randomGames} emptyListMessage={<h1 className="text-4xl font-semibold mb-2 text-center">Find Games</h1>} />
       <GameForm />
     </div>
   );
